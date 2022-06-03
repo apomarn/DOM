e089df41a4bce09fa787d9920567008dedea7dbe
Anghy Pomar <anghypomar@Anghys-MacBook-Pro.local>
2022-06-02 23:20:10 -0400

    complete

---
+++ horoscopos.js
@@ -0,0 +1,62 @@
+let horoscopos = [
+    {
+        name : "Aries",
+        fechas: "21/03 - 12/04",
+
+    },
+    {
+        name : "Tauro",
+        fechas: "20/04 - 20/05",
+        
+    },
+    {
+        name : "Geminis",
+        fechas: "21/05 - 20/06",
+        
+    },
+    {
+        name : "Cancer",
+        fechas: "21/06 - 22/07",
+        
+    },
+    {
+        name : "Leo",
+        fechas: "23/07 - 22/08",
+        
+    },
+    {
+        name : "Virgo",
+        fechas: "23/08 - 22/09",
+        
+    },
+    {
+        name : "Libra",
+        fechas: "23/09 - 22/10",
+        
+    },
+    {
+        name : "Escorpio",
+        fechas: "23/10 - 21/11",
+        
+    },
+    {
+        name : "Sagitario",
+        fechas: "22/11 - 21/12",
+        
+    },
+    {
+        name : "Capricornio",
+        fechas: "22/12 - 20/01",
+        
+    },
+    {
+        name : "Acuario",
+        fechas: "21/01 - 19/02",
+        
+    },
+    {
+        name : "Piscis",
+        fechas: "20/02 - 20/03",
+        
+    }
+]
---
+++ index.js
@@ -0,0 +1,70 @@
+console.log(horoscopos)
+
+let body = document.getElementById("body");
+body.style.display="flex";
+body.style.alignContent="center";
+body.style.flexDirection="column";
+body.style.margin="10px 20px";
+body.style.textAlign= "center";
+
+
+
+let descripcion= document.getElementById("descripcion");
+descripcion.innerHTML = "Como horóscopo se denomina un sistema de predicción del futuro basado en la posición de los astros y los signos del Zodiaco en un momento dado, y en la creencia de cómo esto influye en la vida de las personas.";
+descripcion.style.font = "20px arial";
+
+
+let horoscoposBody = document.createElement("div");
+horoscoposBody.style.display="flex";
+horoscoposBody.style.flexDirection="row";
+horoscoposBody.style.flexWrap="wrap";
+horoscoposBody.style.alignContent="center";
+
+
+for (const horoscopo of horoscopos) {
+    let container = document.createElement("div");
+    container.style.background="black";
+    container.style.width="200px";
+    container.style.height="150px";
+    container.style.padding="10px 10px";
+    container.style.margin="10px";
+    container.style.display="flex";
+    container.style.flexDirection="column";
+    container.style.justifyContent="center";
+
+
+
+  
+
+    container.innerHTML=`
+    <h4 class="name" >${horoscopo.name}</h4>
+    <p class="fecha" >${horoscopo.fechas}</p>
+    `
+    horoscoposBody.append(container);
+
+   
+
+}
+
+
+
+
+
+let horoscoposDiv = document.getElementById("horoscopos");
+horoscoposDiv.append(horoscoposBody);
+
+
+
+let names = document.getElementsByClassName("name");
+for(const name of names) {
+    name.style.font= "bold 30px arial";
+    name.style.color="lightBlue";
+    name.style.margin="0"
+}
+
+let fechas = document.getElementsByClassName("fecha");
+for(const fecha of fechas) {
+    fecha.style.font= "bold 18px arial";
+    fecha.style.color="lightBlue";
+    fecha.style.margin="0"
+}
